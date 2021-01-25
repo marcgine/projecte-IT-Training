0a514c417be162255a36c58b5ec1a286a5ec14ed
joanpardogine <joanpardo@ginebro.cat>
2021-01-25 19:47:22 +0100

    Creada i validada funció passaSeguent

--- js/seguent.js
+++ js/seguent.js
@@ -1,3 +1,17 @@
-function passaSeguent(objecte){
-  alert("Has pitjat el botó seguent!");
+function passaSeguent(objRebut){
+  let idObjPare = document.getElementById(objRebut.parentElement.id);
+  idObjPare.classList.remove("elementVisible");
+  idObjPare.classList.add("elementOcult");
+  
+  let coleccioArticles = document.getElementsByTagName("article");
+  let idObjPreguntaSeguent;
+
+  for (let index = 0; index < coleccioArticles.length; index++) {
+    if(coleccioArticles[index].id==idObjPare.id){
+      idObjPreguntaSeguent=coleccioArticles[index+1].id;
+    };
+  }
+
+  document.getElementById(idObjPreguntaSeguent).classList.add("elementVisible");
+  document.getElementById(idObjPreguntaSeguent).classList.remove("elementOcult");
 }
