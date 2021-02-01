19b395c08134d612291775e30885c587c044c75f
joanpardogine <joanpardo@ginebro.cat>
2021-02-01 18:02:30 +0100

    Creat moviments.js

---
+++ js/moviments.js
@@ -0,0 +1,17 @@
+function passaSeguent(objRebut){
+    let idObjPregAct = objRebut.parentElement.id; 
+    let objPregAct = document.getElementById(idObjPregAct);
+    let colArticles = document.getElementsByTagName("article");
+    let idObjPregSeg;
+      for (let i = 0; i < colArticles.length; i++) {
+      if(colArticles[i].id == idObjPregAct){
+        idObjPregSeg = colArticles[i+1].id;
+        break;
+      };
+    }
+    let objPregSeg = document.getElementById(idObjPregSeg);
+    objPregAct.classList.remove("elementVisible");
+    objPregAct.classList.add("elementOcult");
+    objPregSeg.classList.add("elementVisible");
+    objPregSeg.classList.remove("elementOcult");
+  }
